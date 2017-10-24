7d40d60a902085148fdda99e3f25abb5b5976d4b
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-24 11:12:19 -0700

    Experiment inheritance via ES6 class in playground.js

--- playground.js
+++ playground.js
@@ -1,6 +1,20 @@
 var { Emitter } = require("./emitter");
 
-var myEmitter = new Emitter();
+// var myEmitter = new Emitter();
 
-myEmitter.on("data", () => console.log("data received"));
-myEmitter.emit("data");
+// myEmitter.on("data", () => console.log("data received"));
+// myEmitter.emit("data");
+
+class MyEmitter extends Emitter {
+  storeEvent(type, listener) {
+    this.on(type, listener);
+  }
+  emitEvent(type) {
+    this.emit(type);
+  }
+}
+
+var emitter = new MyEmitter();
+
+emitter.storeEvent("data", () => console.log("data received"));
+emitter.emitEvent("data");
